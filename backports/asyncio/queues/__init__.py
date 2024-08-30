27aec6c65adc3db496f9034e438a1a5d63ef2391
John Parton <john.parton.iv@gmail.com>
2024-08-30 17:02:31 -0500

    Don't automatically try to import asyncio.queues with python >= 3.13. Makes type annotations not work as well.

--- backports/asyncio/queues/__init__.py
+++ backports/asyncio/queues/__init__.py
@@ -7,25 +7,5 @@ __all__ = (
     "QueueShutDown",
 )
 
-import warnings
-
-
-try:
-    from asyncio.queues import (
-        Queue,
-        PriorityQueue,
-        LifoQueue,
-        QueueFull,
-        QueueEmpty,
-        QueueShutDown,
-    )
-
-    warnings.warn(
-        "The 'backports.asyncio.queues' package does nothing on Python 3.13 and later. "
-        "Consider using the 'asyncio.queues' module directly.",
-        DeprecationWarning,
-    )
-
-except ImportError:
-    from asyncio.queues import QueueFull, QueueEmpty
-    from .compat import Queue, PriorityQueue, LifoQueue, QueueShutDown
+from asyncio.queues import QueueFull, QueueEmpty
+from .compat import Queue, PriorityQueue, LifoQueue, QueueShutDown
